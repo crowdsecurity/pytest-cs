1c4e9e77c6067bed2e1c4ad7f19b39ac43a4ca51
Marco Mariani <marco@crowdsec.net>
2023-03-08 14:28:02 +0100

    Configurable timeout

---
+++ src/pytest_cs/helpers.py
@@ -0,0 +1,9 @@
+import os
+
+
+def get_timeout(default=20):
+    t = os.getenv('CROWDSEC_TEST_TIMEOUT', default)
+    try:
+        return int(t)
+    except (ValueError, TypeError):
+        raise Exception(f"Invalid CROWDSEC_TEST_TIMEOUT ({t}): must be an integer")
