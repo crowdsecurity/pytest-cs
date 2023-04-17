7afc70b9ff20957a185ee53ccc8587ce398b78cc
mmetc <92726601+mmetc@users.noreply.github.com>
2023-04-17 16:13:15 +0200

    Fix rpm_package_path fixture; v0.7.1 (#10)

--- tests/test_container.py
+++ tests/test_container.py
@@ -6,7 +6,7 @@ from pytest_cs import Status
 
 def test_log_waiters(container):
     with container(image='hello-world', wait_status=Status.EXITED) as c:
-        for waiter in c.log_waiters(timeout=5):
+        for waiter in c.log_waiters():
             with waiter as matcher:
                 matcher.fnmatch_lines(["Hello from Docker!"])
 
@@ -14,17 +14,17 @@ def test_log_waiters(container):
 @pytest.mark.xfail
 def test_log_waiters_fail(container):
     with container(image='hello-world', wait_status=Status.EXITED) as c:
-        for waiter in c.log_waiters(timeout=5):
+        for waiter in c.log_waiters():
             with waiter as matcher:
                 matcher.fnmatch_lines(["Hullo from Docker!"])
 
 
 def test_wait_for_log(container):
     with container(image='hello-world', wait_status=Status.EXITED) as c:
-        c.wait_for_log("Hello from Docker!", timeout=5)
+        c.wait_for_log("Hello from Docker!")
 
 
 @pytest.mark.xfail
 def test_wait_for_log_fail(container):
     with container(image='hello-world', wait_status=Status.EXITED) as c:
-        c.wait_for_log("Hullo from Docker!", timeout=5)
+        c.wait_for_log("Hullo from Docker!")
